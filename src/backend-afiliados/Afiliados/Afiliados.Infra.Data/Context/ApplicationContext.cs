493d39130aa498ca3cd39bbc15d8974a06c2e02d
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2023-09-19 16:49:31 -0300

    Creation of Context

---
+++ src/backend-afiliados/Afiliados/Afiliados.Infra.Data/Context/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Afiliados.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Afiliados.Infra.Data.Context
+{
+	public class ApplicationContext : DbContext
+	{
+		public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		}
+
+		public DbSet<Producer> Producers { get; set; }
+		public DbSet<Affiliated> Affiliateds { get; set; }
+	}
+}
