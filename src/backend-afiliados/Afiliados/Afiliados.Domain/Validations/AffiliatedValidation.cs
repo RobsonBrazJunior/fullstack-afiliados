6b14d6e9bb93a1a7fdbc450c16ce99b43bd38e65
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2023-09-20 12:00:50 -0300

    Add validations for entities

---
+++ src/backend-afiliados/Afiliados/Afiliados.Domain/Validations/AffiliatedValidation.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Afiliados.Domain.Entities;
+
+namespace Afiliados.Domain.Validations
+{
+	public class AffiliatedValidation : AbstractValidator<Affiliated>
+	{
+		public AffiliatedValidation()
+		{
+			RuleFor(affiliated => affiliated.Name)
+				.NotNull().NotEmpty().WithMessage("Name must be filled in!");
+		}
+	}
+}
---
+++ src/backend-afiliados/Afiliados/Afiliados.Domain/Validations/ProducerValidation.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Afiliados.Domain.Entities;
+
+namespace Afiliados.Domain.Validations
+{
+	public class ProducerValidation : AbstractValidator<Producer>
+	{
+		public ProducerValidation()
+		{
+			RuleFor(producer => producer.Name)
+				.NotNull().NotEmpty().WithMessage("Name must be filled in!");
+		}
+	}
+}
