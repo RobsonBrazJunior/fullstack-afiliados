f006026e7cd31dedf6059cb50663c94caebe691e
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2023-09-25 19:47:46 -0300

    Create SaleDTO

---
+++ src/backend-afiliados/Afiliados/Afiliados.Domain/DTOs/SaleDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Afiliados.Domain.DTOs
+{
+	public class SaleDTO
+	{
+		[Required(ErrorMessage = "A Type is Required")]
+		public byte Type { get; set; }
+
+		[Required(ErrorMessage = "A Date is Required")]
+		public DateTime Date { get; set; }
+
+		[Required(ErrorMessage = "A Product is Required")]
+		public string Product { get; set; }
+
+		[Required(ErrorMessage = "A Value is Required")]
+		public int Value { get; set; }
+
+		[Required(ErrorMessage = "A Seller is Required")]
+		public string Seller { get; set; }
+	}
+}
